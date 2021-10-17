4dfddb56a5c3293ded048b5386d08c7891831069
nobmurakita <nobmurakita@users.noreply.github.com>
2021-10-17 23:33:09 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import { getEvent, deleteEvent, putEvent } from '../actions';
 
@@ -20,10 +22,14 @@ class EventsShow extends Component {
   renderField(field) {
     const { input, label ,type, meta: { touched, error } } = field;
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -40,6 +46,7 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 };
 
     return (
       <React.Fragment>
@@ -48,9 +55,9 @@ class EventsShow extends Component {
           <div><Field label="Body" name="body" type="text" component={this.renderField}/></div>
 
           <div>
-            <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-            <Link to="/" >Cancel</Link>
-            <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
+            <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+            <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" >Cancel</Link>} />
+            <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
           </div>
         </form>
       </React.Fragment>
